70807b37a187801a9e5d9390b9de736dc60a3174
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-08-06 10:26:02 +0530

    removed vite setup

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import TodoReducer from './Actions/todo.action'
-
-export const store = configureStore({
-    reducer: {
-        todo: TodoReducer
-    }
-})
