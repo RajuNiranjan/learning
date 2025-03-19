03751e42a5016d08756e849eaec63e204674c60f
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-03-19 09:39:53 +0530

    Initialized the setup for React

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import TodoReducer from './Actions/todoSlice.action'
-
-export const store = configureStore({
-    reducer: {
-        todo: TodoReducer
-    }
-})
