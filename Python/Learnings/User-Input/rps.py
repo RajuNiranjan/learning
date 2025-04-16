ab874de900dafb58a3021f772c3f7ad965b3d3eb
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-04-16 22:29:17 +0530

    added RPS game in Python/Learnings/User-Input

---
+++ Python/Learnings/User-Input/rps.py
@@ -0,0 +1,32 @@
+import sys
+import random
+from enum import Enum
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+playerChoice = input("Enter ... \n ROCK \n PAPER \n SCISSORS \n\n : ").upper()
+
+if playerChoice not in [e.name for e in RPS]:
+    sys.exit("You must choose ROCK, PAPER or SCISSORS")
+
+playerChoice = RPS[playerChoice]
+computerChoice = random.choice(list(RPS))
+
+print("")
+print("You chose " + playerChoice.name + ".")
+print("The computer chose " + computerChoice.name + ".")
+print("")
+
+if playerChoice == computerChoice:
+    print("It's a tie!")
+elif playerChoice == RPS.ROCK and computerChoice == RPS.SCISSORS:
+    print("You win!")
+elif playerChoice == RPS.PAPER and computerChoice == RPS.ROCK:
+    print("You win!")
+elif playerChoice == RPS.SCISSORS and computerChoice == RPS.PAPER:
+    print("You win!")
+else:
+    print("You lose!")
