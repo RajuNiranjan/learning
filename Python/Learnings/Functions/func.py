7f126a10561b0795413a2e323cf7dbd593f49cef
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-04-19 22:53:08 +0530

    added functions in Python/Learnings/Functions

---
+++ Python/Learnings/Functions/func.py
@@ -0,0 +1,30 @@
+# - FUNCTIONS
+
+# def hello_world():
+#     print("Hello, World!")
+
+# hello_world()
+
+# def sum(num1, num2):
+#     sum = num1 + num2
+#     print(sum)
+
+# print(sum(1, 2))
+# sum(1, 5)
+
+# def multiply(*args):
+#     print(args)
+#     print(type(args))
+    
+
+# multiply(1, 2, 3, 4, 5)
+
+
+def mult(**kwargs):
+    print(kwargs)
+    print(type(kwargs))
+    
+
+mult(num1=1, num2=2, num3=3, num4=4, num5=5)
+
+
