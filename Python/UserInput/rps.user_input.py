0bacb34472c35e870e39ee2d08014720d1edd48e
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-04-24 20:27:07 +0530

    added userInputs with RPS in Python

---
+++ Python/UserInput/rps.user_input.py
@@ -0,0 +1,39 @@
+
+import random
+from enum import Enum
+
+class RPS(Enum):
+    ROCK=1
+    PAPER=2
+    SCISSORS=3
+
+playersChoice = int(input("Enter ...\n1 for Rock \n2 for Paper \n3 for Sciesors \nChoice: "))
+
+if playersChoice < 1 or playersChoice > 3:
+    print("Please choose the values 1, 2, 3 only.")
+
+computerChoice = int(random.choice('123'))
+
+
+print("=====================")
+print("   Game starts Now   ")
+print("=====================")
+print("")
+
+print(f'You Choosen: {RPS(playersChoice).name}')
+print("")
+print(f'You Choosen: {RPS(computerChoice).name}')
+print("")
+print("")
+
+if playersChoice == computerChoice:
+    print("Game Draw 🪢")
+elif (playersChoice == 1 and computerChoice == 3) or (playersChoice == 2 and computerChoice == 1) or (playersChoice == 3 and computerChoice == 2):
+    print("You Win 🥳")
+else: 
+    print("You Lose 🤦‍♂️")
+
+print("")
+print("=====================")
+print("   Game Over Now   ")
+print("=====================")
