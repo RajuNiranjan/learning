88080abc1c7aa98d40d68be55599f66cefd8e617
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-04-25 07:32:30 +0530

    added loops and updated rps with loop in Python

---
+++ Python/Loops/for_loop.py
@@ -0,0 +1,42 @@
+names = ["Deav", "John", "Jane", "Jim", "Jill"]
+
+# for name in names:
+#     print(name)
+
+# for i in "JANUARY":
+#     print(i)
+
+# for i in names:
+#     if i == "Jim":
+#         break
+#     print(i)
+
+# for i in names:
+#     if i == "Jim":
+#         continue
+#     print(i)
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(10, 20):
+#     print(i)
+
+# for i in range(10, 20, 2):
+#     print(i)
+
+# for i in range(5, 0, -1):
+#     for j in range(i, 0, -1):
+#         print("*", end="")
+#     print()  
+
+action = ["eat", "sleep", "repeat"]
+
+# for name in names:
+#     for action in action:
+#         print(f"{name} {action}")
+
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(f"{i} * {j}  = {i * j}")
+    print("--------------------------------")
