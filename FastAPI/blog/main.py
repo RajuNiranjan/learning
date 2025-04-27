de781cb61703486b944399ea6e57697fe72f6d2c
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-04-27 18:20:21 +0530

    added user get, post routes in Python/FastAPI/blog

--- FastAPI/blog/main.py
+++ FastAPI/blog/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException, status
-import schemas
-import models
+import schemas, hashing, models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from typing import List
@@ -55,3 +54,23 @@ def update_blog(id:int, req: schemas.Blog, db: Session = Depends(get_db)):
     blog.body = req.body
     db.commit()
     return blog
+
+
+# ===== USER =====
+
+
+@app.post('/user', status_code=status.HTTP_201_CREATED, response_model=schemas.ShowUser)
+def create_user(req: schemas.User, db: Session= Depends(get_db)):
+    new_user = models.User(name=req.name, email=req.email, password=hashing.Hash.bcrypt(req.password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@app.get('/user/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowUser)
+def get_user(id:int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {id} not found")
+    return user
+
--- FastAPI/blog/schemas.py
+++ FastAPI/blog/schemas.py
@@ -7,4 +7,16 @@ class Blog(BaseModel):
 class ShowBlog(BaseModel):
     title: str
     class Config:
-        orm_mode = True
+        from_attributes = True
+
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    class Config:
+        from_attributes = True
