2cfa6c191147b162c7c800da8a95aae4c9866754
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-04-27 15:17:58 +0530

    added post route for create blogi n FastAPI/blog

--- FastAPI/blog/main.py
+++ FastAPI/blog/main.py
@@ -15,46 +15,10 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/blog", status_code=status.HTTP_201_CREATED)
-def create_blog(blog: schemas.Blog,db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=blog.title, body=blog.body)
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
+def create_blog(req: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=req.title, body=req.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
-
-
-@app.get('/blog', status_code=status.HTTP_200_OK)
-def get_all_blogs(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
-
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
-def get_blog(id, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
-    return blog
-
-
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_blog(id, db: Session = Depends(get_db)):
-    blog=db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
-    db.delete(blog)
-    db.commit()
-    return {"message": "Blog deleted successfully"}
-
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update_blog(id, request: schemas.Blog, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
-    blog.title = request.title
-    blog.body = request.body
-    db.commit()
-    return {"data": {
-        "message": "Blog updated successfully",
-        "blog": blog
-    }}
