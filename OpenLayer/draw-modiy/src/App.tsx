8badbab0ccc7effc94d5d54c241e3ac5f9d35ab2
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-05-07 12:31:55 +0530

    added draw and modify like moving and scalling the shpae added in OpenLayer

---
+++ OpenLayer/draw-modiy/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { MapOne } from "./components/Map_one";
+
+const App = () => {
+  return (
+    <div>
+      <MapOne />
+    </div>
+  );
+};
+
+export default App;
