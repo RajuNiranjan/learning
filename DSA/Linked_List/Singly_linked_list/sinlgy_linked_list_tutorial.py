f062edcd6e1c50c29fe64114f6f1f6f45ea433b0
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-09-10 07:02:51 +0530

    added remove begin in DSA/Linked_List/Singly_linked_list

--- DSA/Linked_List/Singly_linked_list/sinlgy_linked_list_tutorial.py
+++ DSA/Linked_List/Singly_linked_list/sinlgy_linked_list_tutorial.py
@@ -69,6 +69,8 @@ class LinkedList:
         print(f"{x} is not in Linked List")
 
 
+
+
     def insert_empty(self, data):
         new_node = Node(data)
         if self.head is None:
@@ -76,6 +78,13 @@ class LinkedList:
         else:
             print("Linked list is not empty!")
         
+    def remove_begin(self):
+        if self.head is None:
+            print("Linked List is Empty!")
+            return
+        else:
+            print(f'{self.head.data} removed')
+            self.head = self.head.ref
 
 
 
@@ -90,5 +99,6 @@ LL1.add_end(90)
 LL1.add_after(40, 10)
 LL1.add_before(190, 40)
 LL1.insert_empty(1)
+LL1.remove_begin()
 
 LL1.print_LL()
