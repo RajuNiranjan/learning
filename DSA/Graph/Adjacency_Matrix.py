e4376d1eaa56c2daad8d08424da2cb949fa2790c
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-10-04 12:44:09 +0530

    added delete node in adjacency matrix in DSA/Graph/Adjacency_Matrix.py

--- DSA/Graph/Adjacency_Matrix.py
+++ DSA/Graph/Adjacency_Matrix.py
@@ -1,3 +1,11 @@
+from platform import node
+
+
+nodes = []
+graph = []
+node_count = 0
+
+
 def add_node(v):
     global node_count
     if v in nodes:
@@ -23,20 +31,34 @@ def add_edge(v1, v2, cost):
         graph[idx1][idx2] = cost
         graph[idx2][idx1] = cost
 
+
+def delete_node(v):
+    global node_count
+    if v not in graph:
+        return
+    idx1 = nodes.index(v)
+    node_count-=1
+    nodes.remove(v)
+    graph.pop(idx1)
+    for i in graph:
+        i.pop(idx1)
+
 def print_graph():
     for i in range(node_count):
         for j in range(node_count):
             print(graph[i][j], end=' ')
 
-nodes = []
-graph = []
-node_count = 0
 print("Before adding nodes")
 print(nodes)
 print(graph)
 add_node("A")
 add_node("B")
-add_edge("A","B", 5)
+add_node("C")
+add_node("D")
+add_edge("A","B", 15)
+add_edge("C","D", 25)
+add_edge("D","A", 55)
+delete_node("C")
 print("After adding nodes")
 print(nodes)
 print(graph)
