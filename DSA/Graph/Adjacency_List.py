46a55f8acf97b47f7e23627be0c912a213bdb937
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-10-04 13:21:58 +0530

    added delete edge in Adjacency list in DSA/Graph/Adjacency_list

--- DSA/Graph/Adjacency_List.py
+++ DSA/Graph/Adjacency_List.py
@@ -1,44 +1,37 @@
 def add_node(v):
-    if v in graph:
-        return
-    else:
+    if v not in graph:
         graph[v] = []
 
-def add_edge(v1, v2):
-    if v1 not in graph:
+def add_edge(v1, v2, cost=1):
+    if v1 not in graph or v2 not in graph:
         return
-    elif v2 not in graph:
+    if (v2, cost) not in graph[v1]:
+        graph[v1].append((v2, cost))
+    if (v1, cost) not in graph[v2]:
+        graph[v2].append((v1, cost))
+
+def add_weight_edge(v1, v2, cost):
+    add_edge(v1, v2, cost)
+
+def delete_edge(v1, v2):
+    if v1 not in graph or v2 not in graph:
         return
-    else:
-        graph[v1].append(v2)
-        graph[v2].append(v1)
+    graph[v1] = [(n, c) for (n, c) in graph[v1] if n != v2]
+    graph[v2] = [(n, c) for (n, c) in graph[v2] if n != v1]
 
 def delete_node(v):
     if v not in graph:
         return
-    else:
-        graph.pop(v)
-        for i in graph:
-            list1 = graph[i]
-            if v in list1:
-                list1.remove(v)
-
-def add_weight_edge(v1, v2, cost):
-    if v1 not in graph:
-        return
-    elif v2 not in graph:
-        return
-    else:
-        list1 = [v2, cost]
-        list2 = [v1, cost]
-        graph[v1].append(list1)
-        graph[v2].append(list2)
+    for node in list(graph.keys()):
+        graph[node] = [(n, c) for (n, c) in graph[node] if n != v]
+    graph.pop(v)
 
 graph = {}
 add_node("A")
 add_node("B")
 add_node("C")
-add_weight_edge("A","B", 5)
-add_edge("A","C")
+add_weight_edge("A", "B", 5)
+add_edge("A", "C")
 delete_node("C")
+delete_edge("A", "B")
 print(graph)
