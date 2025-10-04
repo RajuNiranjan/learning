42f004a0a95c9b259fd1753f36098357423c4257
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-10-04 12:16:18 +0530

    added edge for Adjacency list in DSA/Graph/Adjacency_List.py

--- DSA/Graph/Adjacency_List.py
+++ DSA/Graph/Adjacency_List.py
@@ -4,9 +4,30 @@ def add_node(v):
     else:
         graph[v] = []
 
+def add_edge(v1, v2):
+    if v1 not in graph:
+        return
+    elif v2 not in graph:
+        return
+    else:
+        graph[v1].append(v2)
+        graph[v2].append(v1)
+
+def add_weight_edge(v1, v2, cost):
+    if v1 not in graph:
+        return
+    elif v2 not in graph:
+        return
+    else:
+        list1 = [v2, cost]
+        list2 = [v1, cost]
+        graph[v1].append(list1)
+        graph[v2].append(list2)
 
 graph = {}
 add_node("A")
 add_node("B")
 add_node("C")
+add_weight_edge("A","B", 5)
+add_edge("A","C")
 print(graph)
